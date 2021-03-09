8d8eb96973f7c0dfcf30cb251efe5eb32d4c8a6a
Adam Hooper <adam@adamhooper.com>
2021-03-09 15:55:35 -0500

    I18nMessage: NamedTuple, not namedtuple

--- cjwmodule/i18n/types.py
+++ cjwmodule/i18n/types.py
@@ -1,20 +1,25 @@
-from collections import namedtuple
+from typing import Dict, Literal, NamedTuple, Union
 
 __all__ = ["I18nMessage"]
 
-I18nMessage = namedtuple("I18nMessage", ["id", "arguments", "source"])
-"""
-A message for Workbench to translate.
 
-This is a data-transfer format for passing messages from modules to Workbench.
-Module authors should invoke :py:func:`i18n.trans()` instead of creating raw
-``I18nMessage`` objects: Workbench's i18n tools parse ``trans()`` calls to
-manipulate ``.po`` files.
+class I18nMessage(NamedTuple):
+    """A message for Workbench to translate.
 
-:param id: String message ID (e.g., "errors.notEnoughColumns").
-:type id: str
-:param arguments: Keyword arguments for the message.
-:type arguments: Dict[str, Union[int, float, str]]
-:param source: Indication of where the message is coming from (`"module"` or `"cjwmodule"`).
-:type source: str
-"""
+    This is a data-transfer format for passing messages between modules and
+    Workbench. Module authors should invoke :py:func:`i18n.trans()` instead of
+    creating raw ``I18nMessage`` objects: Workbench's i18n tools parse
+    ``trans()`` calls to manipulate ``.po`` files.
+    """
+
+    id: str
+    """String message ID (e.g., "errors.notEnoughColumns")."""
+
+    arguments: Dict[str, Union[int, float, str]] = {}
+    """Keyword arguments for the message."""
+
+    source: Literal["module", "cjwmodule", None] = None
+    """Indication of where the message is coming from.
+
+    None means, "Workbench proper.
+    """
